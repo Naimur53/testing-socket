6b3f5b76b930c5f8ecceb9b9aa437d6d33b33f1e
Naimur53 <naimurrhman53@gmail.com>
2022-02-16 15:55:09 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useForm } from "react-hook-form";
+
+
+import io from 'socket.io-client'
+const socket = io.connect('http://localhost:5000/');
 
 function App() {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+
+  socket.on('getMessage', data => {
+    console.log('over mmessage', data);
+  })
+
+  const onSubmit = data => {
+    console.log(data)
+
+    socket.emit('ourMessage', data)
+
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit(onSubmit)}>
+
+        <input {...register("message", { required: true })} />
+        <input type="submit" />
+      </form>
     </div>
   );
 }
